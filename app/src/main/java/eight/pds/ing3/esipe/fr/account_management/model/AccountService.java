426a22e337a4bd3c0c05ddb2fd69cff6b62077bf
anthony.perault <anthony.perault@etu.u-pec.fr>
2017-12-02 13:56:47 +0100

    ajout de la route pour l'authentification

--- app/src/main/java/eight/pds/ing3/esipe/fr/account_management/model/AccountService.java
+++ app/src/main/java/eight/pds/ing3/esipe/fr/account_management/model/AccountService.java
@@ -1,13 +1,28 @@
 package eight.pds.ing3.esipe.fr.account_management.model;
 
 
+
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
 import java.util.List;
 
 import io.reactivex.Observable;
+import okhttp3.OkHttpClient;
+
+import okhttp3.ResponseBody;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 
 /**
@@ -16,16 +31,28 @@ import retrofit2.http.GET;
 
 public interface AccountService {
 
-    String ENDPOINT = "http://192.168.1.44:8080/";
+    String ENDPOINT = "http://192.168.1.13:8181/";
+
+
+    @POST("auth")
+    Observable<ResponseBody> authenticate(@Body Credential credential);
+
 
     @GET("accounts")
-    Observable<List<Account>> getAccounts();
+    Observable<List<Account>> getAccounts(@Header("Authorization") String authHeader);
 
     class Creator {
+
+        //static HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+
+        static OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
 
         public static AccountService newAccountService(){
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(AccountService.ENDPOINT)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build();

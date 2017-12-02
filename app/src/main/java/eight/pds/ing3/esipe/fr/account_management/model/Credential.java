eb8a54545704476a3906c9fa6e755e24545e24fc
anthony.perault <anthony.perault@etu.u-pec.fr>
2017-12-02 14:51:06 +0100

    view et presenter pour l'authentification

--- app/src/main/java/eight/pds/ing3/esipe/fr/account_management/model/Credential.java
+++ app/src/main/java/eight/pds/ing3/esipe/fr/account_management/model/Credential.java
@@ -5,4 +5,23 @@ package eight.pds.ing3.esipe.fr.account_management.model;
  */
 
 public class Credential {
+
+    private String userId;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- app/src/main/java/eight/pds/ing3/esipe/fr/account_management/view/LoginMvpView.java
+++ app/src/main/java/eight/pds/ing3/esipe/fr/account_management/view/LoginMvpView.java
@@ -1,8 +1,16 @@
 package eight.pds.ing3.esipe.fr.account_management.view;
 
+import android.content.Context;
+
 /**
  * Created by antho on 29/11/2017.
  */
 
-public class LoginMvpView {
+public interface LoginMvpView extends MvpView {
+
+
+
+    void startAccountActivity(String token);
+
+    void showLoginError();
 }

d7d4c1757257399fcce78e43d73a649a09d8670e
anthony.perault <anthony.perault@etu.u-pec.fr>
2017-12-03 17:26:26 +0100

    ajout d'une home activity

---
+++ app/src/main/java/eight/pds/ing3/esipe/fr/account_management/view/HomeActivity.java
@@ -0,0 +1,13 @@
+package eight.pds.ing3.esipe.fr.account_management.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+    }
+}

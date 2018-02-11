ef248b07866f55d770252f8458fdf21961f6e115
anthony.perault <anthony.perault@etu.u-pec.fr>
2018-02-11 19:32:27 +0100

    add transaction listing functionality : when an account is selected a new activity list his transactions

---
+++ app/src/main/java/eight/pds/ing3/esipe/fr/account_management/model/TransactionDAO.java
@@ -0,0 +1,8 @@
+package eight.pds.ing3.esipe.fr.account_management.model;
+
+/**
+ * Created by antho on 10/02/2018.
+ */
+
+public class TransactionDAO {
+}
---
+++ app/src/main/java/eight/pds/ing3/esipe/fr/account_management/view/TransactionActivity.java
@@ -0,0 +1,141 @@
+package eight.pds.ing3.esipe.fr.account_management.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import eight.pds.ing3.esipe.fr.account_management.AccountAdapter;
+import eight.pds.ing3.esipe.fr.account_management.R;
+import eight.pds.ing3.esipe.fr.account_management.TransactionAdapter;
+import eight.pds.ing3.esipe.fr.account_management.model.Account;
+import eight.pds.ing3.esipe.fr.account_management.model.Transaction;
+import eight.pds.ing3.esipe.fr.account_management.presenter.TransactionPresenter;
+
+public class TransactionActivity extends AppCompatActivity implements TransactionView {
+
+    private TransactionPresenter presenter;
+    private RecyclerView transactionRecyclerView;
+
+    SearchView searchView;
+    private TextView accountNumberTextView;
+    private TextView accountTypeTextView;
+    private TextView balanceTextView;
+    private TextView todayTextView;
+    private View contentLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction2);
+
+
+
+            String jwtToken = getIntent().getStringExtra("token");
+            Account account = (Account) getIntent().getSerializableExtra("Account");
+
+            accountNumberTextView = findViewById(R.id.text_account_number);
+            accountTypeTextView = findViewById(R.id.text_account_type);
+            balanceTextView = findViewById(R.id.text_balance);
+            todayTextView = findViewById(R.id.text_today);
+
+            searchView = findViewById(R.id.searchView);
+
+
+            contentLayout = findViewById(R.id.name_header2);
+
+            accountNumberTextView.setText("N° " + account.getAccountId());
+            accountTypeTextView.setText(account.getType());
+            balanceTextView.setText(String.valueOf(account.getBalance()));
+
+            SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy");
+            Date today = new Date();
+
+            todayTextView.setText(sf.format(today));
+
+            if (account.getType().equals("CCB")) {
+                contentLayout.setBackgroundColor(Color.parseColor("#D12566"));
+
+            } else {
+                contentLayout.setBackgroundColor(Color.parseColor("#136CBD"));
+
+            }
+
+
+            presenter = new TransactionPresenter();
+            presenter.attachView(this);
+
+
+            transactionRecyclerView = findViewById(R.id.transaction_recycler_view);
+            setupRecyclerView(transactionRecyclerView);
+
+            if(isItFirstTime(account.getAccountId()))
+                presenter.loadTransactions(jwtToken, account);
+            else
+                presenter.loadLocalTransactions(account.getAccountNumber());
+        }
+
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        TransactionAdapter adapter = new TransactionAdapter();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
+    }
+
+    @Override
+    public void showTransactionList(List<Transaction> transactionList) {
+        //Log.d("TransactionActivity","contenu liste transactions" + transactionList.get(0).getDescription());
+        TransactionAdapter adapter = (TransactionAdapter) transactionRecyclerView.getAdapter();
+        adapter.setTransactionList(transactionList);
+        adapter.notifyDataSetChanged();
+        transactionRecyclerView.requestFocus();
+        transactionRecyclerView.setVisibility(View.VISIBLE);
+
+
+    }
+
+
+    private boolean isItFirstTime(String accountNumber){
+        SharedPreferences sharedPreferences = getSharedPreferences("STATE",MODE_PRIVATE);
+        boolean  b = sharedPreferences.getBoolean("state"+accountNumber,true);
+
+        return b;
+    }
+
+
+
+}

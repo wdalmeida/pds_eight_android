db39f529800a7a17fac651bbbee6243df1194f03
warren.dalmeida <warren@local>
2018-03-13 20:25:25 +0100

    Rebase 1 - correction

---
+++ app/src/main/java/eight/pds/ing3/esipe/fr/account_management/view/ListActivityView.java
@@ -0,0 +1,16 @@
+package eight.pds.ing3.esipe.fr.account_management.view;
+
+import java.util.List;
+
+import eight.pds.ing3.esipe.fr.account_management.model.Account;
+
+/**
+ * Created by antho on 12/11/2017.
+ */
+
+public interface ListActivityView extends MvpView {
+
+    void showAccountList(List<Account> accountList);
+
+
+}
